af62692909519ce0bb74db95934d19992c7f10f2
isaacBats <disa.indus@gmail.com>
2016-03-03 02:11:30 -0600

    Creando la clase para la conexion de la base de datos y el modelo completo de usuario

--- src/armaganzasoft/models/User.java
+++ src/armaganzasoft/models/User.java
@@ -1,14 +1,115 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package armaganzasoft.models;
 
+import java.util.Date;
+
 /**
- *
- * @author daniel
+ * @author Isaac Daniel
  */
 public class User {
     
+    private int     id;
+    private int     branch_id;
+    private String  num_employee;
+    private String  name;
+    private String  last_name;
+    private String  email;
+    private String  password;
+    private String  position;
+    private String  roll;
+    private boolean active;
+    private Date    update_at;
+    private Date    created_at;
+
+    public int getId() {
+        return id;
+    }
+
+    
+    public int getBranch_id() {
+        return branch_id;
+    }
+
+    public void setBranch_id(int branch_id) {
+        this.branch_id = branch_id;
+    }
+
+    public String getNum_employee() {
+        return num_employee;
+    }
+
+    public void setNum_employee(String num_employee) {
+        this.num_employee = num_employee;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public void setRoll(String roll) {
+        this.roll = roll;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(Date update_at) {
+        this.update_at = update_at;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
 }
