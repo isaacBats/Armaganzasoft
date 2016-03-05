7f1b112a5956749b002a7af56ec560dc88a60834
isaacBats <disa.indus@gmail.com>
2016-03-05 10:05:49 -0600

    Se agrega test de conexion

--- src/armaganzasoft/ArmaganzaSoft.java
+++ src/armaganzasoft/ArmaganzaSoft.java
@@ -5,47 +5,17 @@
  */
 package armaganzasoft;
 
-import armaganzasoft.services.BaseDatos;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 /**
  *
- * @author daniel
+ * @author isaac daniel
  */
 public class ArmaganzaSoft {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
-        try {
-
-            String sql = "SELECT * FROM users";
-            
-            BaseDatos conn = new BaseDatos();
-            Statement statement = conn.getConnection().createStatement();
-            
-            ResultSet rs;
-            
-            rs = statement.executeQuery(sql);
-            
-            while(rs.next()){
-                System.out.println(rs.getString("name"));
-            }
-            
-            conn.closeConnection();
-            
-            
-        } catch (SQLException ex) {
-            System.out.println("Error: "+ ex );
-        }
-        
+    public static void main(String[] args) {        
                
     }
     
