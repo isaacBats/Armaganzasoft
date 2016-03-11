ae63d9c6e7b3393033cc0426b16340d03b18302f
Erwin Valle <erwinvalleceron@hotmail.com>
2016-03-11 15:18:34 -0600

    Update Valvula.java

--- Valvula.java
+++ Valvula.java
@@ -18,14 +18,15 @@ public class Valvula {
     
     public void open(){
 
-    // the code
+    
  JFrame valvula = new JFrame(); 
         Component panel = null;
 
 valvula.getContentPane().add( panel,BorderLayout.CENTER ); 
     
 valvula.addWindowListener( new WindowAdapter() { 
-@Override
+
+
 public void windowClosing( WindowEvent evt ) { 
 System.exit( 0 ); 
 } 
