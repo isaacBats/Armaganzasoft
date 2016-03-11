4762ef957d2d24cd6c793a1c58a0ee6290f1f6b4
Erwin Valle <erwinvalleceron@hotmail.com>
2016-03-11 15:39:55 -0600

    Added files via upload

---
+++ src/armaganzasoft/Valvula.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package armaganzasoft.components;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.WindowAdapter;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ErwinValle
+ */
+
+
+
+public class Valvula {
+    
+    public void open(){
+
+    // the code
+ JFrame valvula = new JFrame(); 
+        Component panel = null;
+
+valvula.getContentPane().add( panel,BorderLayout.CENTER ); 
+    
+valvula.addWindowListener (new WindowAdapter() {}); { 
+    
+    
+System.exit( 0 ); 
+} 
+}
+        
+
+        }
+
+
+/**
+ *
+ * @author user
+ */
