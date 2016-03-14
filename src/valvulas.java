6f34369211d4e1a7b4dd9220497db5fd93970463
Rodrigo Luna zarate <lunarodrigo66@gmail.com>
2016-03-14 17:08:19 -0600

    Update valvulas.java

--- src/valvulas.java
+++ src/valvulas.java
@@ -4,11 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import static java.lang.System.exit;
 import javax.swing.JFrame;
+import static jdk.nashorn.internal.objects.NativeJava.extend;
 
 /**
  *
- * @author ErwinValle
+ * @author Rodrigo
  */
 
 
@@ -20,20 +22,34 @@ public class Valvula {
     // the code
  JFrame valvula = new JFrame(); 
         Component panel = null;
-        
+       
 
 valvula.getContentPane().add( panel,BorderLayout.CENTER ); 
     
-valvula.addWindowListener (new WindowAdapter() {});  {
-    
-    
-    
-
-System.exit( 0 ); 
-} 
-
-}      
+valvula.addWindowListener (new WindowAdapter() {}); { 
 
         }
+        }
+    public void close(){
+        valvulas.close();
+    }
+    
+    public void estado(){
+        int valvula=0, habilitado, alerta, desabilitado;
+        if(valvula<=4){
+            mensaje.showMessageDialog(mensaje,"valvula encendida", "ENCENDIDA",mensaje.INFORMATION_MESSAGE);
+        }else{
+            if(valvula>=4){
+             mensaje.showMessageDialog(mensaje,"valvula en peligro ", "PELIGRO",mensaje.WARNING_MESSAGE);   
+            }else{
+                mensaje.showMessageDialog(mensaje,"valvula desabilitada","APAGADO",mensaje.ERROR_MESSAGE);
+            }
+        }
+    
+    }
+    private javax.swing.JOptionPane mensaje;
+    }
 
-
+        
+        
+     
