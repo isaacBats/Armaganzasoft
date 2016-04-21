23b42837e19bb24f4d5bf77243fcc50c39e8c068
Rodrigo Luna zarate <lunarodrigo66@gmail.com>
2016-04-21 14:26:08 -0500

    Update ArmaganzaSoft.java

--- src/armaganzasoft/ArmaganzaSoft.java
+++ src/armaganzasoft/ArmaganzaSoft.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package armaganzasoft;
 
-import armaganzasoft.interfaces.Bienvenida;
+import armaganzasoft.interfaces.Login;
 
 /**
  * @author isaac daniel
@@ -19,10 +14,9 @@ public class ArmaganzaSoft {
           
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Bienvenida().setVisible(true);
+                new Login().setVisible(true);
             }
         });
                   
     }
 }
-    
