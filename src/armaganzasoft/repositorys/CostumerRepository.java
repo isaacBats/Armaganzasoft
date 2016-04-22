b0fcd437316614105c28e4facafe233657b759bf
Rodrigo Luna zarate <lunarodrigo66@gmail.com>
2016-04-21 20:15:22 -0500

    Update CostumerRepository.java

--- src/armaganzasoft/repositorys/CostumerRepository.java
+++ src/armaganzasoft/repositorys/CostumerRepository.java
@@ -9,11 +9,11 @@ import java.sql.ResultSet;
  *
  * @author ErwinValle
  */
-public class CostumerRepository extends BaseRepository {
+public class CustomerRepository extends BaseRepository {
     
     private PreparedStatement query;
     
-    public CostumerRepository() {
+    public CustomerRepository() {
         
     }
     
@@ -21,8 +21,7 @@ public class CostumerRepository extends BaseRepository {
         
         try {
             
-            query = con.prepareStatement("INSERT INTO customers (identified, "
-                                                          + "name, "
+            query = con.prepareStatement("INSERT INTO customers (name, "
                                                           + "last_name, "
                                                           + "email, "
                                                           + "telephone, "
@@ -33,22 +32,22 @@ public class CostumerRepository extends BaseRepository {
                                                           + "zip_code, " 
                                                           + "sub_customer, "
                                                           + "customer_id) "
-                                        + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+                                        + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
                                         );
             
             
-            query.setString(1, customer.getIdentified());
-            query.setString(2, customer.getName());
-            query.setString(3, customer.getLast_name());
-            query.setString(4, customer.getEmail());
-            query.setString(5, customer.getTelephone());
-            query.setString(6, customer.getMovil());
-            query.setString(7, customer.getRfc());
-            query.setString(8, customer.getAdress());
-            query.setString(9, customer.getCity());
-            query.setString(10, customer.getZip_code());
-            query.setString(11, customer.getSub_customer());
-            query.setString(12, customer.getCustomer_id());
+          
+            query.setString(1, customer.getName());
+            query.setString(2, customer.getLast_name());
+            query.setString(3, customer.getEmail());
+            query.setString(4, customer.getTelephone());
+            query.setString(5, customer.getMovil());
+            query.setString(6, customer.getRfc());
+            query.setString(7, customer.getAdress());
+            query.setString(8, customer.getCity());
+            query.setString(9, customer.getZip_code());
+            query.setString(10, customer.getSub_customer());
+            query.setString(11, customer.getCustomer_id());
             
             if( !query.execute() ){
                 return true;
@@ -62,36 +61,37 @@ public class CostumerRepository extends BaseRepository {
         }
     }   
      
-     
     public Customer buscarCliente(Customer cliente){
-    
         String where ="";
         ResultSet rs;
         Customer busqueda = new Customer();
-        if(cliente.getIdentified() != null ||
-           cliente.getIdentified() != ""   ||
+        if(cliente.getCustomer_id() != null ||
+           cliente.getCustomer_id() != ""   ||
            cliente.getName()       != null ||
            cliente.getName()       != ""   ||
            cliente.getEmail()      != null ||
            cliente.getEmail()      != ""){
-        where = "WHERE identified LIKE '"+cliente.getIdentified()+"' OR "
+        where = "WHERE customer_id LIKE '"+cliente.getCustomer_id()+"' OR "
               + "name       LIKE '"+cliente.getName()+"' OR "
               + "email      LIKE '"+cliente.getEmail()+"';";
         }
-        
-        try {
+            try {
             query = con.prepareStatement("SELECT * FROM customers "+where);
             rs = query.executeQuery();
             if(rs.next()){
-                //while(rs.next())
-                busqueda.setIdentified(rs.getString("identified"));
+                while(rs.next())             
+                busqueda.setCustomer_id(rs.getString("customer_id"));
                 busqueda.setName(rs.getString("name"));
                 busqueda.setLast_name(rs.getString("last_name"));
+                busqueda.setEmail(rs.getString("email"));
+                busqueda.setTelephone(rs.getString("telephone"));
+                busqueda.setMovil(rs.getString("movil"));
+                busqueda.setRfc(rs.getString("rfc"));
+                
                 //aqui aun pueden incluir mas campos de la tabla costumers
                 return busqueda;
             }
-            
-        } catch (SQLException ex) {
+            } catch (SQLException ex) {
             System.out.println("Erro al consultar un Cliente: "+ex);
         }
         return null;
