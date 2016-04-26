415171c57b38a4ecaa2293592fbf9f22cd45f22a
Rodrigo Luna zarate <lunarodrigo66@gmail.com>
2016-04-26 17:26:43 -0500

    Materiales Repository Actualizado
    Materiales Repository Actualizado

--- src/armaganzasoft/repositorys/MaterialesRepository.java
+++ src/armaganzasoft/repositorys/MaterialesRepository.java
@@ -11,34 +11,31 @@ package armaganzasoft.repositorys;
  */
 
 
-import armaganzasoft.models.Material;
+import armaganzasoft.models.Materials;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class MaterialesRepository extends BaseRepository {
+public class MaterialsRepository extends BaseRepository {
     private PreparedStatement query;
     
-    public MaterialesRepository() {
+    public MaterialsRepository() {
         
     }
     
-     public boolean addMat(Material material){
+     public boolean addMat(Materials materials){
         
         try {
             
-            query = con.prepareStatement("INSERT INTO materials (name "
-                                        +"code)"
-                                        + "VALUES(?,?);"
+            query = con.prepareStatement("INSERT INTO materials (name, "
+                                        +"code,"
+                                        +"value)"
+                                        + "VALUES(?,?,?);"
                                         );
-            query = con.prepareStatement("INSERT INTO material_attribute (value)"                                        
-                                        + "VALUES(?);"
-                                        );
+                       
             
-            
-            
-            query.setString(1, material.getName());
-            query.setString(2, material.getCode());
-            query.setString(3, material.getValue());
+            query.setString(1, materials.getName());
+            query.setString(2, materials.getCode());
+            query.setString(3, materials.getValue());
 
   
             
