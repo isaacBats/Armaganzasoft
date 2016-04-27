8f70408b2320fc3c8ddc5bf6233490c17c32db0e
Erwin Valle <erwinvalleceron@hotmail.com>
2016-04-27 17:51:48 -0500

    Update CustomerRepository.java

--- src/armaganzasoft/repositorys/CustomerRepository.java
+++ src/armaganzasoft/repositorys/CustomerRepository.java
@@ -1,6 +1,6 @@
 package armaganzasoft.repositorys;
 
-import armaganzasoft.models.Customer;
+import armaganzasoft.models.Costumer;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
@@ -9,15 +9,15 @@ import java.sql.ResultSet;
  *
  * @author ErwinValle
  */
-public class CustomerRepository extends BaseRepository {
+public class CostumerRepository extends BaseRepository {
     
     private PreparedStatement query;
     
-    public CustomerRepository() {
+    public CostumerRepository() {
         
     }
     
-     public boolean addCostum(Customer costumer){
+     public boolean addCostum(Costumer costumer){
         
         try {
             
@@ -30,9 +30,9 @@ public class CustomerRepository extends BaseRepository {
                                                           + "address, "
                                                           + "city, "
                                                           + "zip_code, " 
-                                                          + "sub_customer, "
-                                                          + "customer_id) "
-                                        + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?);"
+                                                          + "sub_costumer, "
+                                                          + "costumer_id) "
+                                        + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
                                         );
             
             
@@ -61,20 +61,20 @@ public class CustomerRepository extends BaseRepository {
         }
     }   
      
-     public Customer buscarCliente(String identified){
+     public Costumer buscarCliente(String identified){
         String where ="";
         ResultSet rs;
-        Customer busqueda = new Customer ();
+        Costumer busqueda = new Costumer ();
         if(identified != null || identified != ""){
         where = "WHERE email LIKE '"+identified+"' OR rfc LIKE '"+identified+"' OR costumer_id LIKE '"+identified+"';";
         }
             try {
-            query = con.prepareStatement("SELECT * FROM customers "+where);
+            query = con.prepareStatement("SELECT * FROM costumers "+where);
             rs = query.executeQuery();
           
                 while(rs.next()){
                 
-                busqueda.setSub_costumer(rs.getString("sub_customer"));
+                busqueda.setSub_costumer(rs.getString("sub_costumer"));
                 busqueda.setCostumer_id(rs.getString("costumer_id"));
                 busqueda.setName(rs.getString("name"));
                 busqueda.setLast_name(rs.getString("last_name"));
@@ -97,11 +97,11 @@ public class CustomerRepository extends BaseRepository {
         return null;
     }
     
-    public boolean edit(Customer cliente){
+    public boolean edit(Costumer costumer){
         
         try {
             
-            query = con.prepareStatement("UPDATE customers SET     name         = ?, "
+            query = con.prepareStatement("UPDATE costumers SET     name         = ?, "
                                                            +"    last_name    = ?, "
                                                            +"    email        = ?, "
                                                            +"    telephone    = ?, "
@@ -110,32 +110,32 @@ public class CustomerRepository extends BaseRepository {
                                                            +"    address      = ?, "
                                                            +"    city         = ?, "
                                                            +"    zip_code     = ?, "
-                                                           +"    sub_customer = ?, "
+                                                           +"    sub_costumer = ?, "
                                                            +"    costumer_id  = ? "
                                                            +" WHERE id = ?;");
             
-            query.setString(1, cliente.getName());
-            query.setString(2, cliente.getLast_name());
-            query.setString(3, cliente.getEmail());
-            query.setString(4, cliente.getTelephone());
-            query.setString(5, cliente.getMovil());
-            query.setString(6, cliente.getRfc());
-            query.setString(7, cliente.getAddress());
-            query.setString(8, cliente.getCity());
-            query.setString(9, cliente.getZip_code());
-            query.setString(10, cliente.getSub_costumer());
-            query.setString(11, cliente.getCostumer_id());
-            query.setInt(12, cliente.getId());
+            query.setString(1, costumer.getName());
+            query.setString(2, costumer.getLast_name());
+            query.setString(3, costumer.getEmail());
+            query.setString(4, costumer.getTelephone());
+            query.setString(5, costumer.getMovil());
+            query.setString(6, costumer.getRfc());
+            query.setString(7, costumer.getAddress());
+            query.setString(8, costumer.getCity());
+            query.setString(9, costumer.getZip_code());
+            query.setString(10, costumer.getSub_costumer());
+            query.setString(11, costumer.getCostumer_id());
+            query.setInt(12, costumer.getId());
             
             if( !query.execute() ){
-                System.out.println("Se edito el cliente correctamente");
+                System.out.println("Se modifico el cliente correctamente");
                 return true;
             }
             
             query.close();
             
         } catch (SQLException ex) {
-            System.out.println("Error al editar el cliente: "+ ex);
+            System.out.println("Error al modifico el cliente: "+ ex);
         }
         
         return false;
