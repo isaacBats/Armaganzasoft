d15f69117c0717067056cb9ed5d3074a4eb6129c
Rodrigo Luna zarate <lunarodrigo66@gmail.com>
2016-05-03 20:00:14 -0500

    Correcion al modelo
    Correcion al Modelo

--- src/armaganzasoft/models/Materials.java
+++ src/armaganzasoft/models/Materials.java
@@ -9,10 +9,7 @@ package armaganzasoft.models;
  *
  * @author ErwinValle
  */
-import armaganzasoft.services.BaseDatos;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+
 
 
 public class Materials {
@@ -23,49 +20,21 @@ public class Materials {
     private String tipo;
 
    
-   private Statement stm;
-   private ResultSet rs;
-   
-      private String sql;
-      private BaseDatos db;
-      private Connection conn= null;
-    private Statement stmt;
+ 
       
-      /**
-       * Default Construct 
-       */
       public Materials(){
       
       }
-       public Object[][] ConsultarMaterial(){
-    Object [][] datos =new Object[id][];
-        try {
-            if(conectar()){
-                sql="SELECT *FROM materials";
-                Statement stmt=this.stmt;
-                ResultSet res=stmt.executeQuery(sql);
-                int fila=0;
-                while(res.next()){
-                    for(int columna=0; columna<3; columna++)
-                    datos [fila][columna]= res.getObject(columna+1);
-                    fila ++;
-            }
-                res.close();
-                stmt.close();
-                //desconectar();
-            }
-        } catch (Exception e) {
-            System.out.println("Excepcion al Consultar Material : "+e);
-        }
-        return datos;
-    }
-
-
-  
+    
     
     public int getId() {
         return id;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     
    public String getName() {
         return name;
@@ -97,20 +66,5 @@ public class Materials {
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-    
-    
-    
-    
-   
-   
-    private boolean conectar() {
-        this.db = new BaseDatos();
-        this.conn = db.getConnection();
-        if(this.conn != null){
-            return true;
-        }else{
-            return false;
-        }}
-
-    
+ 
 }
