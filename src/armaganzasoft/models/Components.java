7d949df5149f682eccc00a5e3a2a906658fc91d3
Rodrigo Luna zarate <lunarodrigo66@gmail.com>
2016-05-23 18:33:09 -0500

    Pantalla Componentes
    Pantalla Componentes

---
+++ src/armaganzasoft/models/Components.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package armaganzasoft.models;
+
+/**
+ *
+ * @author rodri
+ */
+
+public class Components {
+    
+   private int     id;
+    private String  name; 
+    private String  code;
+    private String material_uso;
+    private String estado;
+    private String u_medida;
+    private String atributo;
+    private String valor;
+    private String atributo1;
+    private String valor1;
+    private String atributo2;
+    private String valor2;
+    private String atributo3;
+    private String valor3;
+    private String atributo4;
+    private String valor4;
+       
+      /**
+       * Default Construct 
+       */
+           
+      public Components(){
+      
+      }
+  
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+   public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+       public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+    
+        public String getMaterial_uso() {
+        return material_uso;
+    }
+
+    public void setMaterial_uso(String material_uso) {
+        this.material_uso = material_uso;
+    }
+    
+      public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+     
+          public String getAtributo() {
+        return atributo;
+    }
+
+    public void setAtributo(String atributo) {
+        this.atributo = atributo;
+    }
+    
+      public String getU_medida() {
+        return u_medida;
+    }
+
+    public void setU_medida(String u_medida) {
+        this.u_medida = u_medida;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getAtributo1() {
+        return atributo1;
+    }
+
+    public void setAtributo1(String atributo1) {
+        this.atributo1 = atributo1;
+    }
+
+    public String getValor1() {
+        return valor1;
+    }
+
+    public void setValor1(String valor1) {
+        this.valor1 = valor1;
+    }
+
+    public String getAtributo2() {
+        return atributo2;
+    }
+
+    public void setAtributo2(String atributo2) {
+        this.atributo2 = atributo2;
+    }
+
+    public String getValor2() {
+        return valor2;
+    }
+
+    public void setValor2(String valor2) {
+        this.valor2 = valor2;
+    }
+
+    public String getAtributo3() {
+        return atributo3;
+    }
+
+    public void setAtributo3(String atributo3) {
+        this.atributo3 = atributo3;
+    }
+
+    public String getValor3() {
+        return valor3;
+    }
+
+    public void setValor3(String valor3) {
+        this.valor3 = valor3;
+    }
+
+    public String getAtributo4() {
+        return atributo4;
+    }
+
+    public void setAtributo4(String atributo4) {
+        this.atributo4 = atributo4;
+    }
+
+    public String getValor4() {
+        return valor4;
+    }
+
+    public void setValor4(String valor4) {
+        this.valor4 = valor4;
+    }
+        
+    
+}
