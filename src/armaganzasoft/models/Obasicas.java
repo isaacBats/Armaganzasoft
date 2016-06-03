d72d701e76d43784de19376d40e15fc899404270
Rodrigo Luna zarate <lunarodrigo66@gmail.com>
2016-06-03 11:17:02 -0500

    Pantalla Operaciones Basicas
    Pantalla Operaciones Basicas

---
+++ src/armaganzasoft/models/Obasicas.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package armaganzasoft.models;
+
+/**
+ *
+ * @author rodri
+ */
+public class Obasicas {
+    
+    private int id;
+    private String code;
+    private String descripcion;
+    
+
+public Obasicas(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
+
+
