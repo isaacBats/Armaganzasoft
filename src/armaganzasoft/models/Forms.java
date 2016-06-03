e41629e4c48130a1676bbefdb85f54cb6a290e85
Rodrigo Luna zarate <lunarodrigo66@gmail.com>
2016-06-03 11:57:59 -0500

    Pantalla Formulas
    Pantalla Formulas

--- src/armaganzasoft/models/Forms.java
+++ src/armaganzasoft/models/Forms.java
@@ -14,16 +14,44 @@ public class Forms {
      private int     id;
     private String  code;
     private String  name;
-    private String receta;
-    
-    private String operacion;
     private String material_id;
+    private String descripcion;
     private String valor;
-    private String taraje;
-    private String tolerancia;
-    private String velocidad;
-    private String habilitado;
-  
+    private String material_id1;
+    private String descripcion1;
+    private String valor1;
+    private String material_id2;
+    private String descripcion2;
+    private String valor2;
+    private String material_id3;
+    private String descripcion3;
+    private String valor3;
+    private String material_id4;
+    private String descripcion4;
+    private String valor4;
+    private String material_id5;
+    private String descripcion5;
+    private String valor5;
+    private String material_id6;
+    private String descripcion6;
+    private String valor6;
+    private String material_id7;
+    private String descripcion7;
+    private String valor7;
+    private String material_id8;
+    private String descripcion8;
+    private String valor8;
+    private String material_id9;
+    private String descripcion9;
+    private String valor9;
+    private String material_id10;
+    private String descripcion10;
+    private String valor10;
+    private String material_id11;
+    private String descripcion11;
+    private String valor11;
+    
+   
    
         
       /**
@@ -36,6 +64,10 @@ public class Forms {
     
     public int getId() {
         return id;
+    }
+    
+        public void setId(int id) {
+        this.id = id;
     }
     
    public String getCode() {
@@ -54,22 +86,6 @@ public class Forms {
         this.name = name;
     }
     
-        public String getReceta() {
-        return receta;
-    }
-
-    public void setReceta(String receta) {
-        this.receta = receta;
-    }
-    
-       public String getOperacion() {
-        return operacion;
-    }
-
-    public void setOperacion(String operacion) {
-        this.operacion = operacion;
-    }
-    
            public String getMaterial_id() {
         return material_id;
     }
@@ -77,8 +93,64 @@ public class Forms {
     public void setMaterial_id(String material_id) {
         this.material_id = material_id;
     }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion1() {
+        return descripcion1;
+    }
+
+    public void setDescripcion1(String descripcion1) {
+        this.descripcion1 = descripcion1;
+    }
+
+    public String getDescripcion2() {
+        return descripcion2;
+    }
+
+    public void setDescripcion2(String descripcion2) {
+        this.descripcion2 = descripcion2;
+    }
+
+    public String getDescripcion3() {
+        return descripcion3;
+    }
+
+    public void setDescripcion3(String descripcion3) {
+        this.descripcion3 = descripcion3;
+    }
+
+    public String getDescripcion4() {
+        return descripcion4;
+    }
+
+    public void setDescripcion4(String descripcion4) {
+        this.descripcion4 = descripcion4;
+    }
+
+    public String getDescripcion5() {
+        return descripcion5;
+    }
+
+    public void setDescripcion5(String descripcion5) {
+        this.descripcion5 = descripcion5;
+    }
+
+    public String getDescripcion6() {
+        return descripcion6;
+    }
+
+    public void setDescripcion6(String descripcion6) {
+        this.descripcion6 = descripcion6;
+    }
     
-           public String getValor() {
+               public String getValor() {
         return valor;
     }
 
@@ -86,40 +158,226 @@ public class Forms {
         this.valor = valor;
     }
     
-     public String getTaraje() {
-        return taraje;
+    
+    public String getMaterial_id1() {
+        return material_id1;
     }
 
-    public void setTaraje(String taraje) {
-        this.taraje = taraje;
-    }
-    
-       public String getTolerancia() {
-        return tolerancia;
+    public void setMaterial_id1(String material_id1) {
+        this.material_id1 = material_id1;
     }
 
-    public void setTolerancia(String tolerancia) {
-        this.tolerancia = tolerancia;
-    }
-    
-       public String getVelocidad() {
-        return velocidad;
+    public String getValor1() {
+        return valor1;
     }
 
-    public void setVelocidad(String velocidad) {
-        this.velocidad = velocidad;
+    public void setValor1(String valor1) {
+        this.valor1 = valor1;
     }
-    
-      public String getHabilitado(){
-          return habilitado;
-      }
 
-    public void setHabilitado(String habilitado) {
-        this.habilitado = habilitado;
+    public String getMaterial_id2() {
+        return material_id2;
+    }
+
+    public void setMaterial_id2(String material_id2) {
+        this.material_id2 = material_id2;
+    }
+
+    public String getValor2() {
+        return valor2;
+    }
+
+    public void setValor2(String valor2) {
+        this.valor2 = valor2;
+    }
+
+    public String getMaterial_id3() {
+        return material_id3;
+    }
+
+    public void setMaterial_id3(String material_id3) {
+        this.material_id3 = material_id3;
+    }
+
+    public String getValor3() {
+        return valor3;
+    }
+
+    public void setValor3(String valor3) {
+        this.valor3 = valor3;
+    }
+
+    public String getMaterial_id4() {
+        return material_id4;
+    }
+
+    public void setMaterial_id4(String material_id4) {
+        this.material_id4 = material_id4;
+    }
+
+    public String getValor4() {
+        return valor4;
+    }
+
+    public void setValor4(String valor4) {
+        this.valor4 = valor4;
+    }
+
+    public String getMaterial_id5() {
+        return material_id5;
+    }
+
+    public void setMaterial_id5(String material_id5) {
+        this.material_id5 = material_id5;
+    }
+
+    public String getValor5() {
+        return valor5;
+    }
+
+    public void setValor5(String valor5) {
+        this.valor5 = valor5;
+    }
+
+    public String getMaterial_id6() {
+        return material_id6;
+    }
+
+    public void setMaterial_id6(String material_id6) {
+        this.material_id6 = material_id6;
+    }
+
+    public String getValor6() {
+        return valor6;
+    }
+
+    public void setValor6(String valor6) {
+        this.valor6 = valor6;
+    }
+
+    public String getMaterial_id7() {
+        return material_id7;
+    }
+
+    public void setMaterial_id7(String material_id7) {
+        this.material_id7 = material_id7;
+    }
+
+    public String getDescripcion7() {
+        return descripcion7;
+    }
+
+    public void setDescripcion7(String descripcion7) {
+        this.descripcion7 = descripcion7;
+    }
+
+    public String getValor7() {
+        return valor7;
+    }
+
+    public void setValor7(String valor7) {
+        this.valor7 = valor7;
+    }
+
+    public String getMaterial_id8() {
+        return material_id8;
+    }
+
+    public void setMaterial_id8(String material_id8) {
+        this.material_id8 = material_id8;
+    }
+
+    public String getDescripcion8() {
+        return descripcion8;
+    }
+
+    public void setDescripcion8(String descripcion8) {
+        this.descripcion8 = descripcion8;
+    }
+
+    public String getValor8() {
+        return valor8;
+    }
+
+    public void setValor8(String valor8) {
+        this.valor8 = valor8;
+    }
+
+    public String getMaterial_id9() {
+        return material_id9;
+    }
+
+    public void setMaterial_id9(String material_id9) {
+        this.material_id9 = material_id9;
+    }
+
+    public String getDescripcion9() {
+        return descripcion9;
+    }
+
+    public void setDescripcion9(String descripcion9) {
+        this.descripcion9 = descripcion9;
+    }
+
+    public String getValor9() {
+        return valor9;
+    }
+
+    public void setValor9(String valor9) {
+        this.valor9 = valor9;
+    }
+
+    public String getMaterial_id10() {
+        return material_id10;
+    }
+
+    public void setMaterial_id10(String material_id10) {
+        this.material_id10 = material_id10;
+    }
+
+    public String getDescripcion10() {
+        return descripcion10;
+    }
+
+    public void setDescripcion10(String descripcion10) {
+        this.descripcion10 = descripcion10;
+    }
+
+    public String getValor10() {
+        return valor10;
+    }
+
+    public void setValor10(String valor10) {
+        this.valor10 = valor10;
+    }
+
+    public String getMaterial_id11() {
+        return material_id11;
+    }
+
+    public void setMaterial_id11(String material_id11) {
+        this.material_id11 = material_id11;
+    }
+
+    public String getDescripcion11() {
+        return descripcion11;
+    }
+
+    public void setDescripcion11(String descripcion11) {
+        this.descripcion11 = descripcion11;
+    }
+
+    public String getValor11() {
+        return valor11;
+    }
+
+    public void setValor11(String valor11) {
+        this.valor11 = valor11;
     }
     
-     
-    }
+    
+
+}
 
     
       
